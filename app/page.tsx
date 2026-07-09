10b6909e14f9a2aa08b6feb12c2a807771b9be5a
Mateo <r120rmc@gmail.com>
2026-07-09 17:18:24 -0400

    Reordenar home: Colección Origen tras el hero, Nuestra esencia al final

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <EssenceGrid />
       <CollectionsShowcase />
       <FeaturedProducts />
       <HistoryTeaser />
       <CraftsmanshipProcess />
       <Testimonials />
       <InstagramGrid />
+      <EssenceGrid />
     </>
   );
 }
